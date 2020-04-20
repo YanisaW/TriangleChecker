bcf4d4eaa86fd467657f28381767bb842408ab4a
YanisaW <dmcottoncandy255@gmail.com>
2020-04-21 00:42:54 +0700

    add deploy

--- triangle/src/App.js
+++ triangle/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { SketchPicker } from 'react-color';
+//import { SketchPicker } from 'react-color';
 
 export default class App extends Component {
 
