54cee150dbca4982a386baaeb0b26c1856450996
YanisaW <dmcottoncandy255@gmail.com>
2020-04-21 01:13:33 +0700

    Updates

---
+++ precache-manifest.6756c7ef162d849ecfaa76be35989d26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045150817e9e7706eee0d110965cc442",
+    "url": "/TriangleChecker/index.html"
+  },
+  {
+    "revision": "1ae096b781c2c6322f60",
+    "url": "/TriangleChecker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a299dd089dbc16844ff",
+    "url": "/TriangleChecker/static/js/2.a97e2bdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TriangleChecker/static/js/2.a97e2bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae096b781c2c6322f60",
+    "url": "/TriangleChecker/static/js/main.15caf88e.chunk.js"
+  },
+  {
+    "revision": "1b187ab0e6ba59c8eac7",
+    "url": "/TriangleChecker/static/js/runtime-main.2361f471.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TriangleChecker/static/media/logo.5d5d9eef.svg"
+  }
+]);
