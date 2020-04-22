81b7d1deaeace97ef1f591fedbaa692a884ff736
YanisaW <dmcottoncandy255@gmail.com>
2020-04-22 13:54:58 +0700

    add valifdation

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import './App.css';
 import { Helmet } from 'react-helmet';
 import './fonts/LemonMilkMedium-mLZYV.otf'
 
-export default class App extends Component {
+const initialState = {
+  side_1: "",
+  side_2: "",
+  side_3: "",
+  result: "",
+  err1: "",
+  err2: "",
+  err3: "",
+  field: {}
+}
+
+
+
+export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      side_1: 0,
-      side_2: 0,
-      side_3: 0,
-      result: "",
-      err1: "",
-      err2: "",
-      err3: ""
-    };
+    this.state = initialState;
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -32,57 +37,107 @@ export default class App extends Component {
     this.setState({ background: color.hex });
   };
 
-  validate = () => {
+  Validation = () => {
     let err1 =  "";
     let err2 =  "";
     let err3 =  "";
+    let field = this.state.field; 
+   
+
 
     if(!this.state.side_1){
+     
       err1 = 'cannot be empty!';
     }
-    if(err1){
+    if(this.state.side_1 !== ""){
+      if(this.state.side_1 <= 0){
+       
+        err1 = 'cannot be negative or zero';
+      }
+      if(this.state.side_1 > 100){
+        err1 = 'out of bound. max number is 100';
+      }
+    }
+
+    if(!this.state.side_2){
+      err2 = 'cannot be empty!';
+    }
+    if(this.state.side_2 !== ""){
+      if(this.state.side_2 <= 0){
+       
+        err2 = 'cannot be negative or zero';
+      }
+      if(this.state.side_2 > 100){
+        err2 = 'out of bound. max number is 100';
+      }
+    }
+
+    if(!this.state.side_3){
+      err3 = 'cannot be empty!';
+    }
+    if(this.state.side_3 !== ""){
+      if(this.state.side_3 <= 0){
+       
+        err3 = 'cannot be negative or zero';
+      }
+      if(this.state.side_3 > 100){
+        err3 = 'out of bound. max number is 100';
+      }
+    }
+
+    if(err1||err2||err3){
       this.setState({err1});
+      this.setState({err2});
+      this.setState({err3});
       return false;
     }
 
     return true;
   };
+
+  resetForm = () =>{
+    
+    this.setState(initialState);
+  }
   
   handleClick(event) {
-    const isValid = this.validate();
-    
+    event.preventDefault();
+    const isValid = this.Validation();
     var a = eval(this.state.side_1 * this.state.side_1)
     var b = eval(this.state.side_2 * this.state.side_2)
     var c = eval(this.state.side_3 * this.state.side_3)
     
-    
-
-    if(this.state.side_1 >= 0 && this.state.side_2 >= 0 && this.state.side_3 >= 0){
-        if((eval((this.state.side_1*1) + (this.state.side_2*1)) < (this.state.side_3*1))
+    if(isValid){
+     
+      if((eval((this.state.side_1*1) + (this.state.side_2*1)) < (this.state.side_3*1))
         || (eval((this.state.side_2*1) + (this.state.side_3*1)) < (this.state.side_1*1))
         || (eval((this.state.side_1*1) + (this.state.side_3*1)) < (this.state.side_2*1))){
-          this.setState({result: "Not a Triangle"})
+          this.setState({result: "[ Not a Triangle | เป็นสามเหลี่ยมไม่ได้ ]"})
         }
 
         else if((this.state.side_1 === this.state.side_2) && (this.state.side_2 === this.state.side_3)){
-          this.setState({result: "Equilateral Triangle"})
+          this.setState({result: "[ Equilateral Triangle | สามเหลี่ยมด้านเท่า ]"})
         }
    
         else if(((this.state.side_1 === this.state.side_2) && (this.state.side_1 !== this.state.side_3)) 
         || ((this.state.side_2 === this.state.side_3) && (this.state.side_2 !== this.state.side_1))
         || ((this.state.side_1 === this.state.side_3) && (this.state.side_1 !== this.state.side_2))){
-          this.setState({result: "Isosceles Triangle"})
+          this.setState({result: "[ Isosceles Triangle | สามเหลี่ยมหน้าจั่ว]"})
         }
    
         else if((a === eval(b+c)) || (b === eval(a+c)) || (c === eval(a+b))){
-          this.setState({result: "Right Triangle"})
+          this.setState({result: "[ Right Triangle | สามเหลี่ยมุมฉาก ]"})
         }
   
 
         else if((this.state.side_1 !== this.state.side_2) && (this.state.side_2 !== this.state.side_3) && (this.state.side_1 !== this.state.side_3) ){
-        this.setState({result: "Scalene Triangle"})
+        this.setState({result: "[ Scalene Triangle | สามเหลี่ยมด้านไม่เท่า]"})
         }
-      }else {this.setState({result: "Cannont fill"})}
+  
+    }
+    
+  
+    
     
 
     event.preventDefault();
@@ -101,26 +156,28 @@ export default class App extends Component {
           <title>Three Angle</title>
         </Helmet>
         
-        <br></br> <br></br>
-        <h1>Three Angle</h1>
-        <text style= {{fontSize : 20}}>Enter the length of sides</text>
-        <form onSubmit={this.handleSubmit}>
+        <br></br>
+        <text style={{fontSize:50,color: "#FFFAE9",fontWeight:1000}}>Three Angle</text>
+        <br></br>
+        <text style= {{fontSize : 28}}>Enter the length of sides</text>
+        <br></br>--ensure that your fill the number again after you clicked reset button--
+        <form ref={form => this.foem = form}>
 
-          <label>
+          <label >
             <br></br>
             <t>Side1   : </t> 
             <input 
             type = "number" 
             name = "side_1"
             placeholder="Enter Number" 
-            min="0" max="100"
             required
+            value={this.state.field["side_1"]}
             side_1 ={this.state.side_1}
             onChange={this.handleChange} /> 
             <div style={{fontSize: 15,color:'red'}}>{this.state.err1}</div>
           </label>
           
-          <br></br>
+          
 
           <label>
             <br></br>
@@ -130,13 +187,14 @@ export default class App extends Component {
             name = "side_2"
             placeholder="Enter Number" 
             required
+            value={this.state.field["side_2"]}
             side_2 ={this.state.side_2} 
             onChange={this.handleChange} /> 
             <div style={{fontSize: 15,color:'red'}}>{this.state.err2}</div>
           </label>
 
-          <br></br>
-
+         
+         
           <label>
             <br></br>
             <t>Side3   : </t> 
@@ -145,19 +203,23 @@ export default class App extends Component {
               name = "side_3"
               placeholder="Enter Number" 
               required
+              value={this.state.field["side_3"]}
               side_3 ={this.state.side_3} 
               onChange={this.handleChange} /> 
             <div style={{fontSize: 15,color:'red'}}>{this.state.err3}</div>
           </label>
 
-          <br></br><br></br>
+          <br></br>
 
-          <button id = "calculate" type = "button"  onClick = {this.handleClick} className = "App"> Calculate </button>
+          <button id = "calculate" type = "button" onClick = {this.handleClick} className = "App"> Calculate </button>
+          <button id = "clear" type = "button" onClick = {this.resetForm} className = "App"> Reset</button>
+
         </form>
         <label>
             <br></br>
-            <t>Result   : </t><br></br> <br></br> 
-            <label style= {{fontSize: 35,color: "white"}}>{this.state.result}</label>
+            <t>Result   : </t><br></br> 
+            
+           <label style= {{fontSize: 40,color: "#FFFAE9"}}>{this.state.result}</label>
         </label>
 
       
